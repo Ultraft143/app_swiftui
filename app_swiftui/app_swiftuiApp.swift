046608c700d88e53b9fdfd60fa0b082e99006dcb
Gonçalo Silva <goncalodiao@gmail.com>
2024-01-09 11:02:41 -0800

    Initial Commit

---
+++ app_swiftui/app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_swiftuiApp.swift
+//  app_swiftui
+//
+//  Created by Sysprobs on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
